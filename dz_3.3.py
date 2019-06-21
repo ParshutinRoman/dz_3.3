648a7c4293f3783f987d10cf0353481f96a08cf8
Roman <34662053+ParshutinRoman@users.noreply.github.com>
2019-06-22 00:24:26 +0300

    Add files via upload

--- dz_3.3.py
+++ dz_3.3.py
@@ -1,10 +1,10 @@
 from urllib.parse import urlencode
 import requests
 
-APP_ID = 6471121
+APP_ID = 7029454
 AUTHORIZE_URL = 'https://oauth.vk.com/authorize'
 VERSION = '5.95'
-userID1 = 6471121
+userID1 = 1410393
 userID2 = 6471123
 
 auth_params = {
@@ -18,47 +18,31 @@ auth_params = {
 token_link = '?'.join((AUTHORIZE_URL, urlencode(auth_params)))
 print(token_link)
 
-token = '2eb0e9c5198797503fa6fb0c9f5d6e63423e738478b4c9c3e24c2f00964f04f57f8b3870acf6afd3cd0a9'
+token = '73e64e811349b00ae127a08ac05454fb2b4d5384b2ce955285e19398a18caccd6b8185402f0ceb965bb95'
 
 class User():
     def __init__(self, token):
-        self.token = global token
+        self.token = token
 
     def __and__(self, other_user):
         self.other_user = other_user
 
-
-        inp2 = input('введите id пользователя:')
-
         params = {
             'v': VERSION,
-            'source_uid': 6471121,
-            'target_uid': inp2,
+            'source_uid': self,
+            'target_uid': other_user,
             'access_token': token
         }
 
         response = requests.get('https://api.vk.com/method/friends.getMutual', params)
-        response = response.json()['response']
-
-        for id in response:
-            print('id общего друга:', id)
-            print('ссылка на страницу: https://vk.com/id', id, sep='')
+        response = response.json()#['response']
+        print(response)
 
 
+user1 = User(userID1)
+user2 = User(userID2)
 
 
-# user1 = User(user_id_1)
-# user2 = User(user_id_2)
-# #создали пользователей
-#
-# mutal_user_list = user1 & user2
-# print(mutal_user_list)
+mutal_user_list = user1 & user2
+print(mutal_user_list)
 
-
-# class User:
-#     __and__(self, other_user):
-#     # other_user - это другой экземпляр класса user
-#     # далее идет логика получения общего списка друзей
-#     return mutal_user_lust
-#
-# # теперь при применении оператора & к экземпляру класса User будет неявно вызываться метод __and__. Т.е вызвали user1 & user2, а фактически где то под капотом питона вызвалось user1.__and__(user2)
